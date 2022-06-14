ded8db3ff42ca8a3c69977c6f073949d8c910337
Roberto Casula <roberto.casula@injenia.it>
2022-06-14 16:01:03 +0200

    Initial setup of project

---
+++ App/iOS/iOS/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  videocall
+//
+//  Created by Roberto Casula on 14/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# videocall
+
+A description of this package.
