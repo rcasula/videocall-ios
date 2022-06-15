319881cd9e3ac79ab54ecf269542c44e89a76039
Roberto Casula <roberto.casula@injenia.it>
2022-06-15 07:35:04 +0200

    Add shared models module and Contact model

---
+++ Sources/SharedModels/Contact.swift
@@ -0,0 +1,15 @@
+//
+//  Contact.swift
+//  
+//
+//  Created by Roberto Casula on 15/06/22.
+//
+
+import Foundation
+
+public struct Contact {
+
+    public let name: String
+    public let surname: String
+    public let avatarUrl: URL
+}
