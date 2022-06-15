1047b7aef0854df1a3fc8dae8704750043bde308
Roberto Casula <roberto.casula@injenia.it>
2022-06-15 07:36:19 +0200

    Add modules to package

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "videocall",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ContactsClient",
+            targets: ["ContactsClient"]
+        ),
+        .library(
+            name: "ContactsFeature",
+            targets: ["ContactsFeature"]
+        ),
+        .library(
+            name: "SharedModels",
+            targets: ["SharedModels"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ContactsClient",
+            dependencies: []
+        ),
+        .target(
+            name: "ContactsFeature",
+            dependencies: []
+        ),
+        .target(
+            name: "SharedModels",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "ContactsClientTests",
+            dependencies: ["ContactsClient"]
+        ),
+    ]
+)
