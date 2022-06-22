8790b9bf18f167c6569ede4ce8ecc216477ce498
Roberto Casula <roberto@casula.dev>
2022-06-22 07:45:46 +0200

    Add AppFeature

---
+++ Sources/AppFeature/AppCoordinator.swift
@@ -0,0 +1,66 @@
+//
+//  AppCoordinator.swift
+//  
+//
+//  Created by Roberto Casula on 21/06/22.
+//
+
+import UIKit
+import Foundation
+import AuthClient
+import LoginFeature
+import ContactsClient
+import ContactsFeature
+
+public class AppCoordinator: NSObject, Coordinator {
+
+    private let window: UIWindow
+    public var rootViewController: UIViewController
+    private var authClient: AuthClientProtocol
+    private let contactsClient: ContactsClient
+
+    public init(
+        window: UIWindow,
+        authClient: AuthClientProtocol,
+        contactsClient: ContactsClient
+    ) {
+        self.window = window
+        self.authClient = authClient
+        self.contactsClient = contactsClient
+
+        if let _ = authClient.getSavedCredentials() {
+            let controller = ContactsController(
+                authClient: authClient,
+                contactsClient: contactsClient
+            )
+            self.rootViewController = UINavigationController(rootViewController: controller)
+        } else {
+            self.rootViewController = LoginController(authClient: authClient)
+        }
+    }
+
+
+    public func start() {
+        authClient.delegate = self
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
+}
+
+extension AppCoordinator: AuthClientDelegate {
+
+    public func didLogin() {
+        let controller = UINavigationController(
+            rootViewController: ContactsController(
+                authClient: authClient,
+                contactsClient: contactsClient
+            )
+        )
+        window.swapRootViewController(with: controller, animationType: .dismiss)
+    }
+
+    public func didLogout() {
+        let controller = LoginController(authClient: authClient)
+        window.swapRootViewController(with: controller, animationType: .present)
+    }
+}
---
+++ Sources/AppFeature/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Roberto Casula on 21/06/22.
+//
+
+import UIKit
+
+public protocol Coordinator {
+    var rootViewController: UIViewController { get }
+    func start()
+}
