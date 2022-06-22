06e93a36de0ecc31ac3a80de44f6fe79f99acb44
Roberto Casula <roberto@casula.dev>
2022-06-22 08:45:50 +0200

    Use shared extensions

--- Package.swift
+++ Package.swift
@@ -116,6 +116,7 @@ let package = Package(
                 "AuthClient",
                 "ContactsClient",
                 "SharedModels",
+                "SharedExtensions",
             ]
         ),
         .target(
