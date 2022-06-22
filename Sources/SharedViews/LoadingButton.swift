20cecbed1a4847b7a145e4a2537023fcf37d16c7
Roberto Casula <roberto@casula.dev>
2022-06-22 07:35:58 +0200

    Loading button improvements

--- Sources/SharedViews/LoadingButton.swift
+++ Sources/SharedViews/LoadingButton.swift
@@ -10,45 +10,53 @@ import UIKit
 public class LoadingButton: UIButton {
 
     private var originalButtonText: String?
-    private var activityIndicator: UIActivityIndicatorView?
+    private var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.hidesWhenStopped = true
+        return view
+    }()
 
-    public var activityIndicatorColor: UIColor = .lightGray
-
-    func showLoading() {
-        originalButtonText = self.titleLabel?.text
-        self.setTitle("", for: .normal)
-
-        if activityIndicator == nil {
-            activityIndicator = createActivityIndicator()
+    public var activityIndicatorColor: UIColor = .lightGray {
+        didSet {
+            activityIndicator.color = activityIndicatorColor
         }
-
-        showSpinning()
     }
 
-    func hideLoading() {
-        self.setTitle(originalButtonText, for: .normal)
-        activityIndicator.stopAnimating()
-    }
-
-    private func createActivityIndicator() -> UIActivityIndicatorView {
-        let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.color = activityIndicatorColor
-        return activityIndicator
-    }
-
-    private func showSpinning() {
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(activityIndicator)
-        centerActivityIndicatorInButton()
-        activityIndicator.startAnimating()
-    }
-
-    private func centerActivityIndicatorInButton() {
-        guard let activityIndicator = activityIndicator else {
-            return
+    public override var tintColor: UIColor! {
+        didSet {
+            activityIndicatorColor = tintColor
         }
+    }
 
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(activityIndicator)
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public func startLoading() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.originalButtonText = self?.titleLabel?.text
+            self?.setTitle("", for: .normal)
+            self?.isEnabled = false
+            self?.activityIndicator.startAnimating()
+        }
+    }
+
+    public func stopLoading() {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.setTitle(self?.originalButtonText, for: .normal)
+            self?.isEnabled = true
+            self?.activityIndicator.stopAnimating()
+        }
+    }
+
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
             activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor),
