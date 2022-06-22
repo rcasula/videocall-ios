d771849dee0cf2a1e9785d16c05d2c1b067fd97b
Roberto Casula <roberto@casula.dev>
2022-06-22 08:45:43 +0200

    Add some extensions

---
+++ Sources/SharedExtensions/Array+SafeGet.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Roberto Casula on 22/06/22.
+//
+
+import Foundation
+
+public extension Array {
+
+    /// Could be used to retrieve the element at the given index, only if the index is inside the array
+    /// - Parameter index: the index of the element
+    /// - Returns: nil if the index is out of bounds, otherwise the element
+    func safeGetElement(at index: Int) -> Element? {
+        guard indices.contains(index) else { return nil }
+        return self[index]
+    }
+
+    /// Could be used to retrieve the element at the given index, only if the index is inside the array
+    ///  Usage:
+    ///
+    ///     let element = array[safe: 2]
+    /// - Parameter safe: the index of the element
+    /// - Returns: nil if the index is out of bounds, otherwise the element
+    subscript(safe index: Int) -> Element? {
+        return safeGetElement(at: index)
+    }
+}
