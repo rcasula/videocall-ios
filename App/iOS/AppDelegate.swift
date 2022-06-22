51d59c1dce9cdc12fae666e8041cad432dd01307
Roberto Casula <roberto@casula.dev>
2022-06-22 08:45:36 +0200

    Add contacts mock

--- App/iOS/AppDelegate.swift
+++ App/iOS/AppDelegate.swift
@@ -34,7 +34,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             persistenceClient: PersistenceClient(),
             rememberCredentials: true
         )
-        let contactsClient = MockContactsClient()
+        let contactsClient = MockContactsClient(
+            contacts: [
+                .init(
+                    name: "Katherine",
+                    surname: "Wells",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/a?s=50&d=identicon&r=PG")!
+                ),
+                .init(
+                    name: "Samantha",
+                    surname: "Butler",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/b?s=50&d=identicon&r=PG")!
+                ),
+                .init(
+                    name: "Victoria",
+                    surname: "Holt",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/c?s=50&d=identicon&r=PG")!
+                ),
+                .init(
+                    name: "Nathan",
+                    surname: "Gray",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/d?s=50&d=identicon&r=PG")!
+                ),
+                .init(
+                    name: "Jeffrey",
+                    surname: "Cannon",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/e?s=50&d=identicon&r=PG")!
+                ),
+                .init(
+                    name: "James",
+                    surname: "Baldwin",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/f?s=50&d=identicon&r=PG")!
+                ),
+                .init(
+                    name: "Shane",
+                    surname: "Brown",
+                    avatarUrl: URL(string: "https://www.gravatar.com/avatar/g?s=50&d=identicon&r=PG")!
+                )
+            ],
+            enableDelay: true
+        )
         let window = UIWindow(frame: UIScreen.main.bounds)
         let coordinator = AppCoordinator(
             window: window,
