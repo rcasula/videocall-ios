4de9bfa4f63b5d66261049908ab2ac1bb5448915
Roberto Casula <roberto@casula.dev>
2022-06-22 07:46:02 +0200

    Fix project and app target

--- App/iOS/AppDelegate.swift
+++ App/iOS/AppDelegate.swift
@@ -6,20 +6,45 @@
 //
 
 import UIKit
+import AuthClient
+import ApiClient
+import KeychainClientLive
+import ContactsClient
+import PersistenceClient
+import AppFeature
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var coordinator: AppCoordinator?
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        let authClient = AuthClient(
+            apiClient: MockApiClient(
+                enabledUsers: [
+                    "jshier": "123456"
+                ],
+                enableDelay: true
+            ),
+            keychainClient: KeychainClientLive(),
+            persistenceClient: PersistenceClient(),
+            rememberCredentials: true
+        )
+        let contactsClient = MockContactsClient()
         let window = UIWindow(frame: UIScreen.main.bounds)
+        let coordinator = AppCoordinator(
+            window: window,
+            authClient: authClient,
+            contactsClient: contactsClient
+        )
+        self.coordinator = coordinator
         self.window = window
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
+
+        coordinator.start()
         return true
     }
 
