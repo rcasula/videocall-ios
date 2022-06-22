2d6b902006e9f332000a66688084e998c0b23f68
Roberto Casula <roberto@casula.dev>
2022-06-22 07:24:46 +0200

    Login screen

---
+++ Sources/LoginFeature/LoginController.swift
@@ -0,0 +1,66 @@
+//
+//  LoginController.swift
+//  
+//
+//  Created by Roberto Casula on 20/06/22.
+//
+
+import UIKit
+import AuthClient
+
+public class LoginController: UIViewController {
+
+    private lazy var _view: LoginView = { .init() }()
+
+    private let authClient: AuthClientProtocol
+
+    public init(
+        authClient: AuthClientProtocol
+    ) {
+        self.authClient = authClient
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func loadView() {
+        view = _view
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        _view.loginButton.addTarget(self, action: #selector(loginButtonTapped(_:)), for: .touchUpInside)
+    }
+
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        guard let username = _view.usernameField.text,
+              !username.isEmpty,
+              let password = _view.passwordField.text,
+              !password.isEmpty
+        else { return }
+        authClient.login(
+            username: username,
+            password: password
+        ) { [weak self] result in
+            debugPrint(result)
+            switch result {
+            case .success:
+                break
+            case .failure(let error):
+                self?.showError(error: error)
+            }
+        }
+    }
+
+    private func showError(error: Error) {
+        let alertController = UIAlertController(
+            title: "Error", message: error.localizedDescription, preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true)
+    }
+}
